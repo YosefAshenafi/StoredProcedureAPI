8c1f635d1f7eb7033fd7467b7760af4f3bb70393
Yosef <jossyfreelancer@gmail.com>
2025-02-01 19:17:54 +0300

    add search functionality

--- obj/Debug/net8.0/StoredProcedureAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoredProcedureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede0b93f1692f79ff065bab8687aa59875996eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+268210e2a86d82786cbc31ea675ab9b951739394")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
