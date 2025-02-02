a2a017560f7987a8b4f63336ffa35484b51cc1d1
Yosef <jossyfreelancer@gmail.com>
2025-02-02 08:47:35 +0300

    icon size fix

--- obj/Debug/net8.0/StoredProcedureAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoredProcedureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765e2369999c2e57c6e256ee2fc714cf563b0cb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159959dcc44767cdebc589c2920970468ab3ee4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoredProcedureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
